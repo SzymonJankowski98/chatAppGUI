37e733a6d82eced7c5767c83ded33e1a0edca680
Szymon <jankowski.szymon.98@gmail.com>
2021-01-23 14:01:00 +0100

    init

---
+++ ChatApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ChatApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonZaloguj = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonZaloguj
+            // 
+            this.buttonZaloguj.Location = new System.Drawing.Point(134, 209);
+            this.buttonZaloguj.Name = "buttonZaloguj";
+            this.buttonZaloguj.Size = new System.Drawing.Size(168, 27);
+            this.buttonZaloguj.TabIndex = 0;
+            this.buttonZaloguj.Text = "zaloguj";
+            this.buttonZaloguj.UseVisualStyleBackColor = true;
+            this.buttonZaloguj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(134, 183);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(168, 20);
+            this.textBoxLogin.TabIndex = 1;
+            this.textBoxLogin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(131, 167);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(33, 13);
+            this.labelLogin.TabIndex = 2;
+            this.labelLogin.Text = "Login";
+            this.labelLogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 397);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.buttonZaloguj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonZaloguj;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Label labelLogin;
+    }
+}
+
---
+++ ChatApp/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatApp
+{
+    public partial class Form1 : Form
+    {
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                /* retrieve the SocketStateObject */
+                SocketStateObject state = (SocketStateObject)ar.AsyncState;
+                Socket socketFd = state.m_SocketFd;
+
+                /* send data */
+                int size = socketFd.EndSend(ar);
+
+                if (size > 0)
+                {
+                    //setThreadedStatusLabel("Index number sent.");
+                    //setThreadedButton(true);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                //setThreadedStatusLabel("Check \"Server Info\" and try again!");
+                //setThreadedButton(true);
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                /* retrieve the SocketStateObject */
+                SocketStateObject state = (SocketStateObject)ar.AsyncState;
+                Socket socketFd = state.m_SocketFd;
+
+                /* read data */
+                int size = socketFd.EndReceive(ar);
+
+
+                if (size > 0)
+                {
+                    state.m_StringBuilder.Append(Encoding.ASCII.GetString(state.m_DataBuf, 0, size));
+
+                    /* get the rest of the data */
+                    socketFd.BeginReceive(state.m_DataBuf, 0, SocketStateObject.BUF_SIZE, 0, new AsyncCallback(ReceiveCallback), state);
+                }
+                else
+                {
+                    /* all the data has arrived */
+                    if (state.m_StringBuilder.Length > 1)
+                    {
+                        //setThreadedTextBox(state.m_StringBuilder.ToString());
+                        //setThreadedStatusLabel("Done.");
+                        //setThreadedButton(true);
+
+                        /* shutdown and close socket */
+                        socketFd.Shutdown(SocketShutdown.Both);
+                        socketFd.Close();
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                //setThreadedStatusLabel("Check \"Server Info\" and try again!");
+                //setThreadedButton(true);
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                /* retrieve the socket from the state object */
+                Socket socketFd = (Socket)ar.AsyncState;
+
+                /* complete the connection */
+                socketFd.EndConnect(ar);
+
+                /* create the SocketStateObject */
+                SocketStateObject state = new SocketStateObject();
+                state.m_SocketFd = socketFd;
+
+                //setThreadedStatusLabel("Wait! Sending...");
+
+                socketFd.BeginSend(Encoding.ASCII.GetBytes(this.textBoxLogin.Text), 0, this.textBoxLogin.Text.Length, 0, new AsyncCallback(SendCallback), state);
+
+                //setThreadedStatusLabel("Wait! Reading...");
+
+                /* begin receiving the data */
+                socketFd.BeginReceive(state.m_DataBuf, 0, SocketStateObject.BUF_SIZE, 0, new AsyncCallback(ReceiveCallback), state);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                //setThreadedStatusLabel("Check \"Server Info\" and try again!");
+                //setThreadedButton(true);
+            }
+        }
+
+        private void GetHostEntryCallback(IAsyncResult ar)
+        {
+            try
+            {
+                IPHostEntry hostEntry = null;
+                IPAddress[] addresses = null;
+                Socket socketFd = null;
+                IPEndPoint endPoint = null;
+
+                /* complete the DNS query */
+                //hostEntry = Dns.EndGetHostEntry(ar);
+                //addresses = hostEntry.AddressList;
+                IPAddress ipaddress = System.Net.IPAddress.Parse("192.168.2.30");
+
+                /* create a socket */
+                socketFd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                /* remote endpoint for the socket */
+                endPoint = new IPEndPoint(ipaddress, 1024);//Int32.Parse(this.textBoxPort.Text.ToString()));
+
+                //setThreadedStatusLabel("Wait! Connecting...");
+
+                /* connect to the server */
+                socketFd.BeginConnect(endPoint, new AsyncCallback(ConnectCallback), socketFd);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                //setThreadedStatusLabel("Check \"Server Info\" and try again!");
+                //setThreadedButton(true);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //setThreadedButton(false);
+                //setThreadedTextBox("");
+                //setThreadedStatusLabel("Wait! DNS query...");
+
+                if (true)//this.textBoxAddr.Text.Length > 0 && this.textBoxPort.Text.Length > 0 && this.textBoxIndex.Text.Length > 0)
+                {
+                    /* get DNS host information */
+                    Dns.BeginGetHostEntry("192.168.2.30", new AsyncCallback(GetHostEntryCallback), null);
+                }
+                else
+                {
+                    //if (this.textBoxAddr.Text.Length <= 0) MessageBox.Show("No server address!");
+                    //else if (this.textBoxPort.Text.Length <= 0) MessageBox.Show("No server port number!");
+                    //else if (this.textBoxIndex.Text.Length <= 0) MessageBox.Show("No index number!");
+                    //setThreadedButton(true);
+                    //setThreadedStatusLabel("Check \"Server Info\" and try again!");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                //setThreadedStatusLabel("Check \"Server Info\" and try again!");
+                //setThreadedButton(true);
+            }
+        }
+    }
+
+    public class SocketStateObject
+    {
+        public const int BUF_SIZE = 1024;
+        public byte[] m_DataBuf = new byte[BUF_SIZE];
+        public StringBuilder m_StringBuilder = new StringBuilder();
+        public Socket m_SocketFd = null;
+    }
+}
