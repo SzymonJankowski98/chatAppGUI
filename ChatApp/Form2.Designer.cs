f1badbf153ea4d5fc906e5cc8041b7d8bcd3c2ea
pGumowski38 <p.gumowski38@gmail.com>
2021-01-24 11:51:08 +0100

    Merge Form2.Designer

--- ChatApp/Form2.Designer.cs
+++ ChatApp/Form2.Designer.cs
@@ -28,18 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
+
             this.listBoxFriends = new System.Windows.Forms.ListBox();
             this.labelHello = new System.Windows.Forms.Label();
             this.labelAdd = new System.Windows.Forms.Label();
             this.textBoxAdd = new System.Windows.Forms.TextBox();
             this.buttonAdd = new System.Windows.Forms.Button();
-=======
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
->>>>>>> 640ed8c4003ee50e2500ed7f467eba8feff8ab6e
+
             this.SuspendLayout();
             // 
             // listBoxFriends
@@ -58,7 +53,7 @@
             // 
             // labelHello
             // 
-<<<<<<< HEAD
+
             this.labelHello.AutoSize = true;
             this.labelHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.labelHello.Location = new System.Drawing.Point(9, 19);
@@ -94,51 +89,15 @@
             this.buttonAdd.Text = "Add";
             this.buttonAdd.UseVisualStyleBackColor = true;
             this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
-=======
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 34);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(340, 407);
-            this.listBox1.TabIndex = 1;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
->>>>>>> 640ed8c4003ee50e2500ed7f467eba8feff8ab6e
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(290, 448);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(62, 27);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Dodaj";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 449);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(272, 26);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
             // 
             // Form2
             // 
-<<<<<<< HEAD
             this.ClientSize = new System.Drawing.Size(450, 529);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.textBoxAdd);
             this.Controls.Add(this.labelAdd);
             this.Controls.Add(this.labelHello);
             this.Controls.Add(this.listBoxFriends);
-=======
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 486);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label1);
->>>>>>> 640ed8c4003ee50e2500ed7f467eba8feff8ab6e
             this.Name = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -148,9 +107,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
-<<<<<<< HEAD
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBoxFriends;
@@ -158,9 +114,6 @@
         private System.Windows.Forms.Label labelAdd;
         private System.Windows.Forms.TextBox textBoxAdd;
         private System.Windows.Forms.Button buttonAdd;
-=======
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
->>>>>>> 640ed8c4003ee50e2500ed7f467eba8feff8ab6e
+
     }
 }
